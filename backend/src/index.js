9369cb569814220055849f3491e864b44a8059f4
brkybzkrt <b9bozkurt959@gmail.com>
2025-04-18 14:53:21 +0300

    Add cron job to keep server alive and implement health check endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,17 @@ import 'dotenv/config';
 import authRoutes from "./routes/authRoutes.js";
 import movieRoutes from "./routes/movieRoutes.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+job.start();
 app.use(express.json());
 // app.use(express.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Movie Recommender API is running!' });
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/movies",movieRoutes);
---
+++ backend/src/lib/cron.js
@@ -0,0 +1,22 @@
+import cron from "cron";
+import https from "https";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const job = new cron.CronJob("*/1 * * * *", () => {
+  https
+    .get(process.env.API_URL, (res) => {
+      if (res.statusCode === 200) {
+        console.log("Server is running");
+      } else {
+        console.log("Server is not running", res);
+      }
+    })
+    .on("error", (error) => {
+      console.log(error);
+    });
+});
+
+
+export default job;
